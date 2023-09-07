8703e552967604d14d90f59ecab005498b513962
martin1989 <gimenamartinjosefinamia@gmail.com>
2023-09-07 00:08:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContent from './components/ItemListConteiner/ItemListConteiner';
 import "bulma/css/bulma.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="has-text-centered">
+      <div className="App">
         <NavBar />
         <ItemListContent greeting={"Bienvenidos a Juegos GameOver"} />
+      </div>
     </div>
+
+
   );
 }
 
